fa68c5fb4b6cf79d033798cf608d5f426c0739d3
LOL <kostalesev@gmail.com>
2019-09-27 17:43:26 +0300

    Bla bla

--- php/QueryBuilder/QueryBuilder.php
+++ php/QueryBuilder/QueryBuilder.php
@@ -1,38 +1,44 @@
 <?php
+
 class QueryBuilder
 {
     static private $itemsPerPage = 10;
     static private $instance = null;
     private $pdo;
 
-    private function __construct() {
+    private function __construct()
+    {
         $this->pdo = new PDO("mysql:host=127.0.0.1; dbname=comment", "root", "");
     }
 
-    static public function getInstance() {
+    static public function getInstance()
+    {
         if (self::$instance == null) {
             self::$instance = new QueryBuilder();
         }
         return self::$instance;
     }
 
-    public function addComment($data)  {
+    public function addComment($data)
+    {
         $sql = "INSERT INTO comments (username, email, textcomment) values (:username, :email, :textcomment)";
         $statement = $this->pdo->prepare($sql);
         $statement->execute([
-            ':username'    => $data['your_name'],
-            ':email'       => $data['email'],
+            ':username' => $data['your_name'],
+            ':email' => $data['email'],
             ':textcomment' => $data['textcomment'],
         ]);
     }
 
-    public function allComments() {
+    public function allComments()
+    {
         $sql = "SELECT * FROM comments ";
         $result = $this->pdo->query($sql);
         return $result->fetchAll();
     }
 
-    public function getPagesCount() {
+    public function getPagesCount()
+    {
         $sql = "select count(*) from comments";
         $recordsCount = $this->pdo->query($sql)->fetch()[0];
         $pagesCount = $recordsCount / self::$itemsPerPage;
@@ -45,9 +51,10 @@ class QueryBuilder
 
     }
 
-    public function getCommentsByPage($page = 1) {
+    public function getCommentsByPage($page = 1)
+    {
         $offset = self::$itemsPerPage * ($page - 1);
-        $sql = "SELECT * FROM comments LIMIT ". self::$itemsPerPage ." OFFSET ". $offset .";";
+        $sql = "SELECT * FROM comments LIMIT " . self::$itemsPerPage . " OFFSET " . $offset . ";";
         return $this->pdo->query($sql)->fetchAll();
     }
 
